670a9dad0f5057798b69549eacc2d3643224964b
mwangi victor <tmvicbvn@gmail.com>
2020-09-09 11:46:58 +0300

    Update qa_index.py

--- qa_index.py
+++ qa_index.py
@@ -1,28 +1,41 @@
+import json
+import notify2
+
 from cdqa.utils.filters import filter_paragraphs
 from cdqa.download import download_model, download_bnpp_data
 from cdqa.pipeline.cdqa_sklearn import QAPipeline
 
-quiz = ''
-answer = ''
-query = ''
+with open('cfgs.json', 'r') as reader:
+    cfgs = return reader.read()
+configs = json.loads(cfgs)
 
-respodircap, respodatacap, respomodelsdir = data('qa'), data('data'), data('models')
-respodatadir = str(respodircap) + '/' + str(respodatacap) + '/'
+question = configs['question']
 
-# download_squad(dir = './' + str(respodatadir))
-download_bnpp_data(dir = './' + str(respodatadir))
-# download_model('distilbert-squad_1.1', dir = './' + str(respomodelsdir))
-download_model('bert-squad_1.1', dir = './' + str(respomodelsdir)) 
+data_directory = '/data/'
+models_directory = '/models/'
 
-df = pandas.read_csv(str(respodatadir) + str(respodatacap) + '.csv', converter = {'paragraphs': ast.literal_evl})
+# download_squad(dir = './' + data_directory)
+download_bnpp_data(dir = './' + data_directory)
+# download_model('distilbert-squad_1.1', dir = './' + models_directory)
+download_model('bert-squad_1.1', dir = './' + models_directory)
+
+df = pandas.read_csv(data_directory + '/bnpp_paribas/-??-.csv', converter = {'paragraphs': ast.literal_evl})
 df = filter_paragraphs(df)
-cdqa_pipeline = QAPipeline(reader = str(respomodelsdir) + '/bert_qa.joblib')
+cdqa_pipeline = QAPipeline(reader = models_directory + '/bert_qa/bert_qa.joblib')
 cdqa_pipeline.fit_retriever(q = df) 
 # cdqa_pipeline.fit_reader('path to squad like dataset . json')
-prediction = cdqa_pipeline.predict(q = query, n_prediction = ?) # ? = predictions
+prediction = cdqa_pipeline.predict(q = question, n_prediction = ?) # ? = predictions
 # cdqa_pipeline.dump_reader('path to save . joblib') # save reader model
 
-query = 'query: {}\n'.format(query)
-answer = 'answer: {}\n'.format(prediction[0])
-titile = 'title: {}\n'.format(prediction[1])
+query = 'query: {}\n'.format(query),
+answer = 'answer: {}\n'.format(prediction[0]),
+title = 'title: {}\n'.format(prediction[1]),
 paragraph = 'paragraph: {}\n'.format(prediction[2])
+
+result = query, answer, title, paragraph
+
+notify2.init('question answer')
+notif = notify2.Notification('qa', result)
+# notif.set_urgency(notify2.URGENCY_CRITICAL)
+notif.show()
+notif.set_timeout(10)
